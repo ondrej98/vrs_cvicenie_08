5d78c3aa69714819dadbe1b16b6d0c34731847ba
ondrej98 <ondrejdurmis@gmail.com>
2021-12-01 17:01:22 +0100

    Preparation of font table template.

---
+++ Core/Inc/fonts.h
@@ -0,0 +1,13 @@
+/*
+ * font.h
+ *
+ *  Created on: Dec 1, 2021
+ *      Author: Ondrej
+ */
+
+#ifndef INC_FONTS_H_
+#define INC_FONTS_H_
+#include "main.h"
+
+
+#endif /* INC_FONTS_H_ */
---
+++ Core/Src/fonts.c
@@ -0,0 +1,28 @@
+/*
+ * fonts.c
+ *
+ *  Created on: Dec 1, 2021
+ *      Author: Ondrej
+ */
+#include "fonts.h"
+
+//
+//  Font data for Courier New 12pt
+//
+
+const uint8_t Font_Table[] = {
+// 1 row = abcdefg(dt) [0b00000000]
+		//Expects 10(digits) + 24(chars) + 1(_)
+		0x00,// 0
+		0x00, // 1
+		0x00, // 2
+		0x00, // 3
+		0x00, // 4
+		0x00, // 5
+		0x00, // 6
+		0x00, // 7
+		0x00, // 8
+		0x00, // 9
+		0x00, // A
+		};
+
